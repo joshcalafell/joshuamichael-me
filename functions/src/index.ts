7c0af91f74b7d9fe8f728e87451dbf61a56b322f
rabbitfighter81 <joshuamichaelwaggoner@gmail.com>
2021-09-12 08:24:26 -0700

    Updated to remove eslint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,9 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Joshua!");
-});
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
