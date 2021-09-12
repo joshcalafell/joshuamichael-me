4dee280fe7344931ca6160ce933912850f2b168e
rabbitfighter81 <joshuamichaelwaggoner@gmail.com>
2021-09-12 08:26:36 -0700

    removed functions

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloWorld = void 0;
-const functions = require("firebase-functions");
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Joshua!");
-});
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
 //# sourceMappingURL=index.js.map
